e1bfbbc7d48a238313a63349c8df45ddef16ab98
Artem Poliakov <artem4250@gmail.com>
2022-02-23 21:51:25 +0200

    tests init

---
+++ tests/Geometry_test.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+#include "../Geometry.h"
+#include "gtest/gtest.h"
+/*
+TEST(erjg, euhurg)
+{
+    Circle a{{1.44,-1.44},16}, b{{14,-12.5},15};
+    std::vector<Point> res{{17.022, 2.192}, {-0.192,-17.357}};
+    EXPECT_EQ(a&&b, res);
+}
+ */
---
+++ tests/Graph_test.cpp
@@ -0,0 +1,3 @@
+#include <vector>
+#include "../Graph.h"
+#include "gtest/gtest.h"
